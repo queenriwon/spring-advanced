030d72bf3b65e5c1f4f31abc96589b97e7d3a11e
queenriwon <queenriwon3@gmail.com>
2025-02-26 18:21:50 +0900

    Refactor: lv.5-1-1 관심사 분리

--- src/main/java/org/example/expert/config/AdminAuthInterceptor.java
+++ src/main/java/org/example/expert/config/AdminAuthInterceptor.java
@@ -1,16 +1,11 @@
 package org.example.expert.config;
 
-import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.auth.exception.AuthException;
 import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Slf4j
 public class AdminAuthInterceptor implements HandlerInterceptor {
